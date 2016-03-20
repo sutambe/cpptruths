4317ea5a4a6a8eb78e6413d306f2de4e61babc17
Sumant Tambe <sumant@rti.com>
2016-03-19 19:43:52 -0700

    update

---
+++ algo/condition_var.c
@@ -0,0 +1,84 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+// When using condition variables there is always a Boolean predicate involving
+// shared variables associated with each condition wait that is true if the
+// thread should proceed. Spurious wakeups from the pthread_cond_timedwait() or
+// pthread_cond_wait() functions may occur. Since the return from
+// pthread_cond_timedwait() or pthread_cond_wait() does not imply anything about
+// the value of this predicate, the predicate should be re-evaluated upon such
+// return
+
+typedef enum { First, Second, Third, None } Turn;
+Turn turn = None;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t doneFirst = PTHREAD_COND_INITIALIZER, 
+               doneSecond = PTHREAD_COND_INITIALIZER, 
+               doneThird = PTHREAD_COND_INITIALIZER;
+
+void *first (void *args)
+{
+  while(1)
+  {
+    pthread_mutex_lock(&lock);
+    // Without the turn-while loop, the program blocks after some time.
+    while(turn != First)
+      pthread_cond_wait(&doneThird, &lock);
+    printf("first\n");
+    turn = Second;
+    pthread_cond_signal(&doneFirst);
+    pthread_mutex_unlock(&lock);
+  }
+}
+void *second (void *args)
+{
+  while(1)
+  {
+    pthread_mutex_lock(&lock);
+    while(turn != Second)
+      pthread_cond_wait(&doneFirst, &lock);
+    printf("second\n");
+    turn = Third;
+    pthread_cond_signal(&doneSecond);
+    pthread_mutex_unlock(&lock);
+  }
+}
+void *third (void *args)
+{
+  while(1)
+  {
+    pthread_mutex_lock(&lock);
+    while(turn != Third)
+      pthread_cond_wait(&doneSecond, &lock);
+    printf("third\n");
+    turn = First;
+    pthread_cond_signal(&doneThird);
+    pthread_mutex_unlock(&lock);
+  }
+}
+
+int main ()
+{
+  char buf[32];
+
+  pthread_t t1,t2,t3;
+  pthread_create(&t3, NULL, third, NULL);
+  pthread_create(&t2, NULL, second, NULL);
+  pthread_create(&t1, NULL, first, NULL);
+  sleep(1);
+  
+  pthread_mutex_lock(&lock);
+  printf("Start\n");
+  sleep(1);
+  turn = First;
+  pthread_cond_signal(&doneThird);
+  pthread_mutex_unlock(&lock);
+  
+  printf("Enter \"end\" to end...\n");
+  while(strcmp(gets(buf), "end") != 0);
+
+  return 0;
+}
---
+++ algo/producer_consumer.c
@@ -0,0 +1,194 @@
+/*
+ *	File	: pc.c
+ *
+ *	Title	: Demo Producer/Consumer.
+ *
+ *	Short	: A solution to the producer consumer problem using
+ *		pthreads.	
+ *
+ *	Long 	:
+ *
+ *	Author	: Andrae Muys
+ *
+ *	Date	: 18 September 1997
+ *
+ *	Revised	:
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define QUEUESIZE 10
+#define LOOP 20
+
+void *producer (void *args);
+void *consumer (void *args);
+
+typedef struct {
+	int buf[QUEUESIZE];
+	long head, tail;
+	int full, empty;
+	pthread_mutex_t *mut;
+	pthread_cond_t *notFull, *notEmpty;
+} queue;
+
+queue *queueInit (void);
+void queueDelete (queue *q);
+void queueAdd (queue *q, int in);
+void queueDel (queue *q, int *out);
+
+int main ()
+{
+	queue *fifo;
+	pthread_t pro, con;
+
+	fifo = queueInit ();
+	if (fifo ==  NULL) {
+		fprintf (stderr, "main: Queue Init failed.\n");
+		exit (1);
+	}
+	pthread_create (&pro, NULL, producer, fifo);
+	pthread_create (&con, NULL, consumer, fifo);
+	pthread_join (pro, NULL);
+	pthread_join (con, NULL);
+	queueDelete (fifo);
+
+	return 0;
+}
+
+void *producer (void *q)
+{
+	queue *fifo;
+	int i;
+
+	fifo = (queue *)q;
+
+	for (i = 0; i < LOOP; i++) {
+		pthread_mutex_lock (fifo->mut);
+		while (fifo->full) {
+			printf ("producer: queue FULL.\n");
+			pthread_cond_wait (fifo->notFull, fifo->mut);
+		}
+		queueAdd (fifo, i);
+		pthread_mutex_unlock (fifo->mut);
+		pthread_cond_signal (fifo->notEmpty);
+		usleep (100000);
+	}
+	for (i = 0; i < LOOP; i++) {
+		pthread_mutex_lock (fifo->mut);
+		while (fifo->full) {
+			printf ("producer: queue FULL.\n");
+			pthread_cond_wait (fifo->notFull, fifo->mut);
+		}
+		queueAdd (fifo, i);
+		pthread_mutex_unlock (fifo->mut);
+		pthread_cond_signal (fifo->notEmpty);
+		usleep (200000);
+	}
+	return (NULL);
+}
+
+void *consumer (void *q)
+{
+	queue *fifo;
+	int i, d;
+
+	fifo = (queue *)q;
+
+	for (i = 0; i < LOOP; i++) {
+		pthread_mutex_lock (fifo->mut);
+		while (fifo->empty) {
+			printf ("consumer: queue EMPTY.\n");
+			pthread_cond_wait (fifo->notEmpty, fifo->mut);
+		}
+		queueDel (fifo, &d);
+		pthread_mutex_unlock (fifo->mut);
+		pthread_cond_signal (fifo->notFull);
+		printf ("consumer: recieved %d.\n", d);
+		usleep(200000);
+	}
+	for (i = 0; i < LOOP; i++) {
+		pthread_mutex_lock (fifo->mut);
+		while (fifo->empty) {
+			printf ("consumer: queue EMPTY.\n");
+			pthread_cond_wait (fifo->notEmpty, fifo->mut);
+		}
+		queueDel (fifo, &d);
+		pthread_mutex_unlock (fifo->mut);
+		pthread_cond_signal (fifo->notFull);
+		printf ("consumer: recieved %d.\n", d);
+		usleep (50000);
+	}
+	return (NULL);
+}
+
+#if 0
+typedef struct {
+	int buf[QUEUESIZE];
+	long head, tail;
+	int full, empty;
+	pthread_mutex_t *mut;
+	pthread_cond_t *notFull, *notEmpty;
+} queue;
+#endif
+
+queue *queueInit (void)
+{
+	queue *q;
+
+	q = (queue *)malloc (sizeof (queue));
+	if (q == NULL) return (NULL);
+
+	q->empty = 1;
+	q->full = 0;
+	q->head = 0;
+	q->tail = 0;
+	q->mut = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+	pthread_mutex_init (q->mut, NULL);
+	q->notFull = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+	pthread_cond_init (q->notFull, NULL);
+	q->notEmpty = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+	pthread_cond_init (q->notEmpty, NULL);
+	
+	return (q);
+}
+
+void queueDelete (queue *q)
+{
+	pthread_mutex_destroy (q->mut);
+	free (q->mut);	
+	pthread_cond_destroy (q->notFull);
+	free (q->notFull);
+	pthread_cond_destroy (q->notEmpty);
+	free (q->notEmpty);
+	free (q);
+}
+
+void queueAdd (queue *q, int in)
+{
+	q->buf[q->tail] = in;
+	q->tail++;
+	if (q->tail == QUEUESIZE)
+		q->tail = 0;
+	if (q->tail == q->head)
+		q->full = 1;
+	q->empty = 0;
+
+	return;
+}
+
+void queueDel (queue *q, int *out)
+{
+	*out = q->buf[q->head];
+
+	q->head++;
+	if (q->head == QUEUESIZE)
+		q->head = 0;
+	if (q->head == q->tail)
+		q->empty = 1;
+	q->full = 0;
+
+	return;
+}
