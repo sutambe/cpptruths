2dd48f170882c801b38d411497154b60bdf89c20
Sumant Tambe <sutambe@yahoo.com>
2018-08-20 19:39:57 -0700

    robust typeid

--- cpp0x/robust_typeid/robust_typeid.cpp
+++ cpp0x/robust_typeid/robust_typeid.cpp
@@ -11,10 +11,10 @@ int main(void) {
   getcwd(cwd, sizeof(cwd));
   std::string path = std::string(cwd) + "/libfoo.so";
   void *handle = dlopen(path.c_str(), RTLD_LAZY |RTLD_LOCAL);
+
   std::cout << "handle = " << handle << "\n";
   using FooType = void (*)(const std::type_info &); 
   FooType foo_ptr = reinterpret_cast<FooType>(dlsym(handle, "foo"));
-  //FooType foo_ptr = reinterpret_cast<FooType>(dlsym(handle, "_Z3fooRKSt9type_info"));
 
   if(foo_ptr) 
     foo_ptr(typeid(Foo));
