4f72021509c7ba8c1e4564b7bf9ecbc134d4ab2f
Sumant Tambe <sutambe@yahoo.com>
2019-03-24 20:28:51 -0700

    barebones vcpkg_test cmake project

---
+++ cpp0x/vcpkg_test/include/driver.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <boost/optional.hpp>
+struct Generator {	static boost::optional<int> get_even_random_number() {		int i = std::rand();		return (i % 2 == 0) ? i : optional<int>{};	}};
