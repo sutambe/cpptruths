a7594dec110da4c2964fea93e6d4fdfc4c785c99
Sumant Tambe <sutambe@yahoo.com>
2019-03-25 07:01:30 -0700

    Updated vcpkg_test project structure

--- cpp0x/vcpkg_test/include/driver.h
+++ cpp0x/vcpkg_test/include/driver.h
@@ -1,4 +1,10 @@
 #pragma once
 
-#include <boost/optional.hpp>
-struct Generator {	static boost::optional<int> get_even_random_number() {		int i = std::rand();		return (i % 2 == 0) ? i : optional<int>{};	}};
+#include <boost/optional.hpp>
+
+struct Generator {
+  static boost::optional<int> get_even_random_number() {	
+  int i = std::rand();
+  return (i % 2 == 0) ? i : optional<int>{}	
+  }
+};
